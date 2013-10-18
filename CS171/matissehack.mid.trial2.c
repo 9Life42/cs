777d177031765c17f3031e8a7798c6ae6d84ba76
Matisse Hack <olaf.hastein@gmail.com>
2013-10-18 11:27:49 -0700

    sync

---
+++ CS171/matissehack.mid.trial2.c
@@ -0,0 +1,34 @@
+#include <FPT.h>
+
+//Insert 'X' after every second consonant
+
+int main()
+{
+double keepAlive=0;
+while (keepAlive == 0){
+
+  char in[100], out[100], second;
+  int i;
+
+  cout << "Enter a word: ";
+
+  cin >> word[100];
+
+  i = 0;
+
+  second = -1;
+
+  while (i < 100 && word[i] != '\0') {
+
+    if (word[i] == 'a' || word[i] == 'e' || word[i] == 'i' || word[i] == 'o' || word[i] == 'u') {
+
+    } else {
+
+      second = -1 * second;
+
+    }
+
+    if (second == 1) {}
+
+}
+}
