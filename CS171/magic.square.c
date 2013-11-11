2eb36aafba7ad91e8ddd4bb86e43263279e9cbdb
Matisse Hack <olaf.hastein@gmail.com>
2013-11-11 11:23:09 -0800

    sync

---
+++ CS171/magic.square.c
@@ -0,0 +1,40 @@
+#include <FPT.h>
+
+// Test a magic square (no int is used twice and sum of rows, columns, diagonals the same)
+
+int sum(int n)
+{
+
+  int sum = 0;
+
+  while (n > 0)
+    {
+      sum += n;
+
+      n--;
+
+    }
+
+  return sum;
+
+}
+
+int main()
+{ double keepAlive = 0; while (keepAlive == 0) {
+
+    double r;
+
+    cin >> r;
+
+    double c = r;
+
+    double magic[r][c], i, k, square, sum;
+
+    square = r * c;
+
+    i = 0;
+
+    k = 0;
+
+  }
+}
