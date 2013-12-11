e15d61b9b4f4116cb23db208ac2d08c96d4ee7ff
Matisse Hack <olaf.hastein@gmail.com>
2013-12-11 03:37:09 -0800

    sync

--- CS171/lab11.cone.rotate.c
+++ CS171/lab11.cone.rotate.c
@@ -1,39 +1,34 @@
 #include <FPT.h>
 
-// Draws a rotating cylinder movie
+// Draws a rotating cone movie with one slice a different color
 
 int main()
 {
-	double swidth, sheight ;
+  double swidth, sheight ;
   swidth = 601 ;  sheight = 601 ;
   G_init_graphics (swidth, sheight);
 
   int res, numpoints;
 
- 	double r, x[3], y[3], p, i, k;
+  double r, x[3], y[3], p, i, k;
 
- 	k = 0;
+  k = 0;
 
- 	res = 47;
+  res = 47;
 
- 	p = M_PI / (res / 2);
+  p = (M_PI / (res / 2));
 
   numpoints = 3;
 
- 	while (true) {
+  while (true) {
 
-		G_rgb(1, 1, 1);
-   	G_clear();
-		G_rgb(0, 0, 0);
+    G_rgb(1, 1, 1);
+    G_clear();
+    G_rgb(0, 0, 0);
 
-    i = 1
-    ;
+    i = 0;
 
     while(i < res) {
-
-      x[0] = 300;
-
-      y[0] = 150;
 
       x[1] = 240 * cos((i + 1 + k/4) * p) + 300;
 
@@ -42,25 +37,15 @@ int main()
       x[2] = 240 * cos((i + k/4) * p) + 300;
 
       y[2] = 80 * sin((i + k/4) * p) + 450;
-
-      if (sin((i + 1 + k/4) * p) < -0.17 && sin((i + k/4) * p) < -0.17) {
-
-        G_fill_polygon (x, y, numpoints);
-
-      }
-
-      i+=2;
-
-    }
-
-    i = 0;
-
-    while (i < res) {
 
       x[0] = 300;
 
       y[0] = 450;
 
+      G_fill_polygon (x, y, numpoints);
+
+      i++;
+
       x[1] = 240 * cos((i + 1 + k/4) * p) + 300;
 
       y[1] = 80 * sin((i + 1 + k/4) * p) + 450;
@@ -69,21 +54,57 @@ int main()
 
       y[2] = 80 * sin((i + k/4) * p) + 450;
 
-      G_fill_polygon (x, y, numpoints);
+      x[0] = 300;
 
-      i+=2;
+      y[0] = 150;
+
+      if (y[1] < 436 && y[2] < 436) {
+
+        G_fill_polygon (x, y, numpoints);
+
+      }
+
+      i++;
 
     }
 
-   	k++;
+    G_rgb(1, 0, 0);
 
-   	i = G_wait_key();
+    x[1] = 240 * cos((1 + k/4) * p) + 300;
 
-   	if (i == 'q') {
+    y[1] = 80 * sin((1 + k/4) * p) + 450;
 
-   		break;
+    x[2] = 240 * cos((k/4) * p) + 300;
 
-   	}
+    y[2] = 80 * sin((k/4) * p) + 450;
+
+    x[0] = 300;
+
+    y[0] = 450;
+
+    G_fill_polygon (x, y, numpoints);
     
+    if (y[1] < 436 && y[2] < 436) {
+
+      x[0] = 300;
+
+      y[0] = 150;
+
+      G_fill_polygon (x, y, numpoints);
+
+    }
+    
+    G_rgb(0, 0, 0);
+
+    k++;
+
+    i = G_wait_key();
+
+    if (i == 'q') {
+
+      break;
+
+    }
+
   }
 }
