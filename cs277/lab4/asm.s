f78336db88c18a50fd5217e4ddce67f9c8cd11f1
Matisse Hack <olaf.hastein@gmail.com>
2014-03-12 18:48:15 -0700

    sync

---
+++ cs277/lab4/asm.s
@@ -0,0 +1,11 @@
+	.text
+.globl is_correct
+	.type	is_correct, @function
+is_correct:
+
+   /*  your code should go here */
+
+
+	.size	is_correct, .-is_correct
+	.ident	"GCC: (Ubuntu 4.4.3-4ubuntu5.1) 4.4.3"
+	.section	.note.GNU-stack,"",@progbits
